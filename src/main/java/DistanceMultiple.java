71dc77a3b8519ec883436685272ef8f357afc7c9
mxendzov <mxendzov17@gmail.com>
2025-09-15 13:48:46 -0400

    Commit Distance

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -1,39 +1,41 @@
-/*
- * TODO: APCS-A Distance Program
- *
- * Write a program that:
- *  1. Asks the user how many inputs (pairs of points) they want to enter.
- *  2. For each input, prompt them to enter two points in the format:
- *         (x1,y1)(x2,y2)
- *     Example: (2,3)(7,3)
- *
- *  3. Use Scanner with a custom delimiter:
- *     This should ignore parentheses, commas, and spaces so only numbers remain.
- *
- *  4. Extract x1, y1, x2, y2 as doubles.
- *
- *  5. Compute the distance between the two points:
- *
- *  6. Print the results
- * 
- *  7. Repeat until all inputs have been processed.
- *
- * Example Inputs:
- *  How many inputs? 3
- *  Enter input 1 (like (x1,y1)(x2,y2)): (2,3)(7,3)
- *  Enter input 2 (like (x1,y1)(x2,y2)): (4,5)(8,8)
- *  Enter input 3 (like (x1,y1)(x2,y2)): (-1,-2)(0,7)
- *
- */
-
-
+// NAME : Michael Xendzov
+// GROUP : APCS - A 6th Hour
+// PROBLEM ID : 2.8 Distance Formula
+// PROBLEM DESCRIPTION : Prompt the user for coordinate points
+// : and display the distance
+// LAST MODIFIED DATE : September 11th 2025
+// SOURCES I USED : Java Book
+// PEOPLE I HELPED : Conner Chan, Adam Sagin, Shuler Wiegerink
+// PEOPLE WHO HELPED ME : Conner Chan, Adam Sagin, Shuler Wiegerink
 import java.util.Scanner;
-
-
-public class DistanceMultiple {
-    public static void main(String[] args) {
-
-
+public class DistanceCalc
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+// signature
+        System.out.print("Michael Xendzov\nAPCS-A\n2.8 Distance Formula Program\n\n");
+// prompt the user for number of runs
+                System.out.print("Enter the number of runs: ");
+        int NumRuns = sc.nextInt();
+        sc.nextLine();
+// set delimiter which splits on commas, parentheses, and spaces
+        sc.useDelimiter("[(),\\s]+");
+// loop for the number of runs
+        for (int i = 1; i <= NumRuns; i++)
+        {
+            System.out.print("\nInput the numbers in (x,y) ");
+// give each number a corresponding variable
+            double x1 = sc.nextDouble();
+            double y1 = sc.nextDouble();
+            double x2 = sc.nextDouble();
+            double y2 = sc.nextDouble();
+// calculate distance between the points and output it
+            double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 -
+                    y1, 2));
+            System.out.printf("\nThe distance between the two points is " +
+                    distance);
+        }
+        sc.close();
     }
-
 }
